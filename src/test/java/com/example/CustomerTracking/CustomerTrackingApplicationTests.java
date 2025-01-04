35ab97bef8a4f50ec9be60243740766e39985878
YasinAkdeniz <yasnakdeniz@gmail.com>
2025-01-04 16:37:02 +0300

    upload project

---
+++ src/test/java/com/example/CustomerTracking/CustomerTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomerTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
